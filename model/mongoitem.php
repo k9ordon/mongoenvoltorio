bd13ce3eb67452d056778aedd5ade45d0845aaef
k9ordon <k9ordon@gmail.com>
2013-07-14 16:31:47 +0200

    item isUnique(field), loadExternal()

--- model/mongoitem.php
+++ model/mongoitem.php
@@ -6,42 +6,43 @@ class Model_MongoItem extends Model_Mongo {
 
 	protected $dirtyFields = array();
 
+	protected $externals = array();
+	protected $externalObjects = array();	
+
 	public function __construct($collectionName = false, $data = array()) {
 		parent::__construct();
 
 		if(!$collectionName) throw new Exception("No collection selected");
 
-		// convert constructor (int)id
+		// convert constructor (int)id to $data[_id]
 		if(is_int($data)) {
 			$data = ['_id' => $data];
 		}
 
 		$this->collectionName = $collectionName;
 		$this->collection = $this->db->$collectionName;
-		$this->data = $data;
-
+		
 		// if only _id => load record
 		if(array_key_exists('_id', $data) && count($data) === 1) {
 			$record = $this->collection->findOne(['_id' => $data['_id']]);
 			if(!$record)  throw new Exception("Cannot load record with id " . $data['_id']);
 
 			else $this->data = $record;
+		} else {
+			$this->data = $data;
 		}
-
-		// if _id => update
-		elseif(array_key_exists('_id', $data)) {+
-			$this->update();
-		}
-
-		// weak record
-		else {}
-
 		return $this;
 	}
 
 	// data getter
 	public function __get($field) {
-		if(array_key_exists($field, $this->data)) return $this->data[$field];
+		// return externalObject
+		if(array_key_exists($field, $this->externalObjects)) return $this->externalObjects[$field];
+		// late load external 
+		elseif(array_key_exists($field, $this->externals)) return $this->loadExternal($field);
+		// return data field
+		elseif(array_key_exists($field, $this->data)) return $this->data[$field];
+		// fail
 		else throw new Exception("Cannot get " . $field);
 	}
 
@@ -49,6 +50,20 @@ class Model_MongoItem extends Model_Mongo {
 	public function __set($field, $value) {
 		$this->data[$field] = $value;
 		$this->setDirty($field, $value);
+	}
+
+	public function loadExternal($field) {
+		$external = $this->externals[$field];
+		// load single model
+		if(array_key_exists('id', $external)) {
+			$this->externalObjects[$field] = $this->loadModel($external['model'], $this->data[$external['id']]);
+		}
+		// load model array
+		else {
+			$this->externalObjects[$field] = $this->loadModels($external['model'], $this->data[$external['id']]);
+		}
+
+		return $this->externalObjects[$field];
 	}
 
 	private function setDirty($field, $value) {
@@ -62,10 +77,11 @@ class Model_MongoItem extends Model_Mongo {
 
 	protected function validate() {}
 
-	public function save() {
-		// must validate
-		$this->validate();
+	public function isUnique($field) {
+		return (bool) $this->collection->findOne([$field => $this->$field], ['_id']);
+	}
 
+	public function save() {
 		// update if id
 		if(!array_key_exists('_id', $this->data)) {
 			$this->create();
@@ -74,11 +90,14 @@ class Model_MongoItem extends Model_Mongo {
 		else {
 			$this->update();
 		}
+
+		return $this;
 	}
 
 	// create
 	public function create() {
 		$this->beforeCreate();
+		$this->validate();
 		// unique next id
 		if(!array_key_exists('_id', $this->data)) 
 			$this->data['_id'] = $this->getNextSequence('_id');
@@ -96,6 +115,7 @@ class Model_MongoItem extends Model_Mongo {
 	// update
 	public function update() {
 		$this->beforeUpdate();
+		$this->validate();
 		$this->collection->save($this->data);
 		$this->afterUpdate();
 	}
