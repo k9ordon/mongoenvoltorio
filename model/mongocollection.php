0db80ef36ff601d699127c31c9f772661b50ded0
k9ordon <k9ordon@gmail.com>
2013-07-14 14:46:03 +0200

    collection find()

--- model/mongocollection.php
+++ model/mongocollection.php
@@ -2,14 +2,26 @@
 
 class Model_MongoCollection extends Model_Mongo {
 
-	public function __construct($collectionName = false) {
+	protected $modelType = null;
+
+	protected $cursor;
+
+	public function __construct($collectionName = false, $modelType = false) {
 		parent::__construct();
 
 		if(!$collectionName) throw new Exception("No collection selected");
 
 		$this->collectionName = $collectionName;
-		$this->collection = $this->db->$collection;
+		$this->collection = $this->db->$collectionName;
+		$this->modelType = $modelType;
 
 		return $this;
+	}
+
+	public function findOne() {}
+
+	public function find($query = array(), $fields = array()) {
+		$this->cursor = new Model_MongoCursor($this->collection->find($query, $fields), $this->modelType);
+		return $this->cursor;
 	}
 }
